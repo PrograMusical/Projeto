390a0db07138962808f3ba7978cc1038de25c2fd
PrograMusical <musical.programacao.ifc@gmail.com>
2024-08-04 16:41:01 -0300

    Update parabens_pra_voce_comFit.py
    adicionei as palmas

--- Exercicios/Encontro 1/parabens_pra_voce_comFit.py
+++ Exercicios/Encontro 1/parabens_pra_voce_comFit.py
@@ -11,7 +11,6 @@ sol = PROGRAMUSICAL_NOTA_SOL
 la = PROGRAMUSICAL_NOTA_LA
 si = PROGRAMUSICAL_NOTA_SI
 
-
 # C C D C F E
 fitMedia(do, 1, 1, 1.5)
 fitMedia(do, 1, 1.5, 1.7)
@@ -33,7 +32,7 @@ fitMedia(fa, 1, 6.7, 7)
 fitMedia(la, 1, 7.3, 7.8)
 fitMedia(la, 1, 7.8, 8)
 fitMedia(do, 2, 8, 8.5)
-setEffect(2, PITCHSHIFT, PITCHSHIFT_SHIFT, 12)
+setEffect(2, PITCHSHIFT, PITCHSHIFT_SHIFT, 12, 8, 12, 11)
 setEffect(2, VOLUME, GAIN, 3)
 fitMedia(la, 1, 8.5, 8.8)
 fitMedia(fa, 1, 8.8, 9)
@@ -41,14 +40,24 @@ fitMedia(mi, 1, 9, 9.3)
 fitMedia(re, 1, 9.3, 10)
 
 # B B A F G F F
-fitMedia(si, 3, 11, 11.5)
-fitMedia(si, 3, 11.5, 11.7)
-setEffect(3, PITCHSHIFT, PITCHSHIFT_SHIFT, -1)
-setEffect(3, VOLUME, GAIN, 3)
+fitMedia(si, 2, 11, 11.5)
+fitMedia(si, 2, 11.5, 11.7)
+setEffect(2, PITCHSHIFT, PITCHSHIFT_SHIFT, -1, 11, -1, 14)
+setEffect(2, VOLUME, GAIN, 3)
 fitMedia(la, 1, 11.7, 12)
 fitMedia(fa, 1, 12, 12.5)
 fitMedia(sol, 1, 12.5, 13)
 fitMedia(fa, 1, 13, 13.3)
 fitMedia(fa, 1, 13.3, 14)
+
+# palmas
+palmas = [AK_UNDOG_CLAPS_SNAPS_2, AK_UNDOG_CLAPS_SNAPS_1, AK_UNDOG_CLAPS_SNAPS_3, CIARA_SET_PERC_CLAP_1]
+
+fitMedia(palmas[3], 7, 1, 14)
+fitMedia(palmas[0], 4, 1.02, 14)
+fitMedia(palmas[1], 5, 1.04, 14)
+fitMedia(palmas[2], 6, 1.06, 14)
+
+
 
 
