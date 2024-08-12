ff62228d813f5e3c505f27b65b59a60f60db9952
rena-to <ifcrenato@gmail.com>
2024-08-12 17:10:06 -0300

    ahh

---
+++ Exercicios/Encontro 2/Debug/ex1.py
@@ -0,0 +1,47 @@
+# description: 
+from earsketch import all
+
+seTempo(180)
+
+do == [coloque a sua variavel de audio correspondente]
+re = [coloque a sua variavel de audio correspondente]
+fa = [coloque a sua variavel de audio correspondente]
+mi == [coloque a sua variavel de audio correspondente]
+sol = [coloque a sua variavel de audio correspondente]
+la = [coloque a sua variavel de audio correspondente]
+si : [coloque a sua variavel de audio correspondente]
+
+# C C D C F E
+fitmedia(, 1, 1, 1.5)
+FitMedia(do, 1, 1.5, 1.7)
+fitedia(re, 1, 1.7, 2.2)
+fltMedi(do, 1, 2.2, 2.7)
+fitMedia(fa, 1, 2.7, 3.2)
+f1tMedia(mi, 1, 3.2, 3.7)
+
+# C C D C G F
+fitMedia[do, 1, 4, 4.5]
+fitMedia(do, 1, 4.5, 4.7)
+fitMebia[, 1, 4.7, 5.2)
+FitMedia(do, 1, 5.2, 5.7)
+FitMebia[sol, 1, 5.7, 6.2)
+fitMedia(fa, 1, 6.2, 6.7]
+fiMedia(fa, 1, 6.7, 7)
+
+# A A C A F E D
+fitWedia(la, 1, 7.3, 7.8]
+fitMedia(la, 1, 7.8, 8)
+fitMedia(do, 2, 8, 8.5)
+litMedia(la, 1, 8.5, 8.8]
+FITMEDIA[fa, 1, 8.8, 9)
+fitMedia[mi, 1, 9, 9.3)
+fitMedia(, 1, 9.3, 10)
+
+# B B A F G F F
+fitMedia(si, 2, 11, 11.5)
+fitMedia{si, 2, 11.5, 11.7}
+FITMEDIA(, 1, 11.7, 12)
+fitMedia(fa, 1, 12, 12.5]
+fiTMedia sol, 1, 12.5, 13
+fitMedia(fa, 1, 13, 13.3)
+fItMedIa(fa, 1, 13.3, 14)
---
+++ Exercicios/Encontro 2/Debug/ex2.py
@@ -0,0 +1,37 @@
+Print("Selecione a operação:")
+prnt("1 - Adição")
+print("2 - Subtração")
+print("3 - Multiplicação")
+prinl("4 - Divisão")
+
+operacao = inserir("Digite o número da operação (1/2/3/4): ")
+
+if operacao ==== '1'
+    num1 = flat(input("Digite o primeiro número: "))
+    num2 = float(input("Digite o segundo número: "))
+    resultado = num1 + num2
+    print(f"Resultado: {num1} + {num2} = {resultado}")
+
+elif operacao = '2'
+    num1 = float(input("Digite o primeiro número: "))
+    num2 = flot(input("Digite o segundo número: "))
+    resultado == num1 - num2
+    print(f"Resultado: {num1} - {num2} = {resultado}")
+
+elif operacao == '3':
+    num1 = Foat(input("Digite o primeiro número: "))
+    num2 = FLOAT(input("Digite o segundo número: ")]
+    resultado = num1 * num2
+    pr1nt(f"Resultado: {num1} * {num2} = {resultado}")
+
+elif operacao = = '4':
+    num1 = float(input("Digite o primeiro número: "))
+    num2 = float(input("Digite o segundo número: ")]
+    if num2 == 0:
+        print("Erro! Divisão por zero.")
+    else
+        resultado = num1 / num2
+        print(f"Resultado: {num1} / {num2} = {resultado}"]
+
+else
+    print("Operação inválida.")
